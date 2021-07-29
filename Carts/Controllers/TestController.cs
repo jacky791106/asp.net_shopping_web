1c225ee67680f9f2fdacf127ed5b8d578508a4ca
Jacky <jacky791106@hotmail.com>
2021-07-30 00:06:55 +0800

    add Cart

---
+++ Carts/Controllers/TestController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carts.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult GetCart()
+        {
+            //取得目前購物車
+            var cart = Models.Cart.Operation.GetCurrentCart();
+
+            //如果購物車內無任何商品，則新增一筆
+            if (cart.cartItems.Count == 0)
+            {
+                cart.cartItems.Add(
+                    new Models.Cart.CartItem()
+                    {
+                        Id = 1,
+                        Name = "測試",
+                        Quantity = 1,
+                        Price = 100m
+                    });
+            }
+            else //若有商品，則將第一筆商品數量+1
+            {
+                cart.cartItems.First().Quantity += 1;
+            }
+
+            //回傳目前購物車中的商品總價
+            return Content(string.Format("目前購物車總共: {0}元", cart.TotalAmount));
+        }
+
+
+
+
+
+    }
+}
